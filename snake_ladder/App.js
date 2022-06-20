756fbae7c01bc768d09b31a4132e25792c106b5b
Sarang <sarang@clayfin.com>
2022-06-20 10:16:46 +0530

    ...

---
+++ snake_ladder/App.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text>Srng 0702 mlg mh ind earth universe galaxy</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
