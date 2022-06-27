cf2f659af800fd48865c4ce8c1ed364fb6f2a0be
Sarang <sarang@clayfin.com>
2022-06-27 13:12:12 +0530

    ...

--- snake_ladder/components/Start.js
+++ snake_ladder/components/Start.js
@@ -36,24 +36,27 @@ const styles = StyleSheet.create({
   top:
   {
     marginTop:100,
-    borderWidth:1,
-    borderColor:"red",
+    borderWidth:2,
+    borderColor:"#0488c9",
     borderTopStartRadius:35,
-    borderTopEndRadius:35,    
+    borderTopEndRadius:35, 
+    backgroundColor: "#c9edff",
   } ,
   middle : 
   {
     height: "30%",    
-    width: "2%", 
+    width: "4%", 
     marginLeft:"49%",       
     borderWidth:1,
-    borderColor:"red"
+    borderColor:"#0488c9",
+    backgroundColor: "#c9edff",
   },
   bottom : 
   {
-    borderWidth:1,
-    borderColor:"red",
+    borderWidth:2,
+    borderColor:"#0488c9",
     borderBottomLeftRadius:35,
     borderBottomRightRadius:35,
-  }    
+    backgroundColor: "#c9edff",     
+ }    
 })
