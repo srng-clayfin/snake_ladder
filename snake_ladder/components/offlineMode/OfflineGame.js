5a2016088ba4300ef24f8da35d90fa10a9c0a0ea
Sarang <sarang@clayfin.com>
2022-07-04 09:51:35 +0530

    ...

--- snake_ladder/components/offlineMode/OfflineGame.js
+++ snake_ladder/components/offlineMode/OfflineGame.js
@@ -143,8 +143,8 @@ const OfflineGame = ({navigation}) => {
             setPlace2(place2+diceno);         
         }
 
-        // player1();
-        // player2();
+        player1();
+        player2();
 
         
         if(place1 >= 100)
